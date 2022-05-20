2459da54493f201bd6b3ca0aabab61ac60fb8117
ChuProgram <sungbong.chu@bellevuecollege.edu>
2022-05-19 22:30:36 -0700

    Pushing html, css, & javascript for Part 2 of Project

--- index.js
+++ index.js
@@ -1,104 +1,254 @@
 // start by creating data so we don't have to type it in each time
-let movieArray = [];
+let restaurantArray = [];
+let updatedRestaurantArray = [];
 
-// define a constructor to create movie objects
-let MovieObject = function (pTitle, pYear, pGenre, pMan, pWoman, pURL) {
-    this.Title = pTitle;
-    this.Year = pYear;
-    this.ID = movieArray.length + 1;
-    this.Genre = pGenre;  // action  comedy  drama  horrow scifi  musical  western
-    this.Man = pMan;
-    this.Woman = pWoman;
+// define a constructor to create restaurant objects
+let RestaurantObject = function (pName, pDate, pCity, pStyle, pMenu, pPrice, pURL, pNewDate, pNewMenu) {
+    this.Name = pName;
+    this.Date = pDate;
+    this.ID = 1000 + restaurantArray.length + 1;
+    this.City = pCity;
+    this.Style = pStyle;
+    this.Menu = pMenu;
+    this.Price = pPrice;
     this.URL = pURL;
+    this.NewDate = pNewDate;
+    this.NewMenu = pNewMenu;
 }
 
-let selectedGenre = "not selected";
+let UpdatedRestaurantObject = function (pNewDate, pNewMenu) {
+    this.NewDate = pNewDate;
+    this.NewMenu = pNewMenu;
+}
+
+restaurantArray.push(new RestaurantObject("Pizza Hut", "01/23/2009", "Redmond", "American", "Pepperoni Pizza", "$$$", ""));
+restaurantArray.push(new RestaurantObject("Chipotle", "01/02/2017", "Seattle", "Mexican", "Steak Burrito", "$", "www.asdas.com"));
+restaurantArray.push(new RestaurantObject("Five Guys", "04/04/2019", "Issaquah", "American", "Cheeseburger w/ bacon", "$$", "https://www.fiveguys.com/"));
+
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    createList();
+    //createList();
 
-// add button events ************************************************************************
-    
-    document.getElementById("buttonAdd").addEventListener("click", function () {
-        movieArray.push(new MovieObject(document.getElementById("title").value, document.getElementById("year").value,
-            selectedGenre, document.getElementById("man").value, document.getElementById("woman").value));
-        document.location.href = "index.html#ListAll";
-        // also add the URL value
-    });
-    
-
-    document.getElementById("buttonClear").addEventListener("click", function () {
-        document.getElementById("title").value = "";
-        document.getElementById("year").value = "";
-        document.getElementById("man").value = "";
-        document.getElementById("woman").value = "";
-        document.getElementById("URL").value = "";
+    // add button event
+    document.getElementById("addBtn").addEventListener("click", function () {
+        restaurantArray.push(new RestaurantObject(document.getElementById("name").value, document.getElementById("date").value,
+            document.getElementById("city").value, document.getElementById("cuisine").value, document.getElementById("menuTried").value,
+            document.getElementById("select-price").value, document.getElementById("website").value));
+        document.location.href = "index.html#listPage";
+        console.log(restaurantArray);
     });
 
-    $(document).bind("change", "#select-genre", function (event, ui) {
-        selectedGenre = $('#select-genre').val();
+    // clear button event
+    document.getElementById("clearBtn").addEventListener("click", function () {
+        document.getElementById("name").value = "";
+        document.getElementById("date").value = "";
+        document.getElementById("city").value = "";
+        document.getElementById("cuisine").value = "";
+        document.getElementById("menuTried").value = "";
+        document.getElementById("select-price").value = "blank";   // why not working?????
+        document.getElementById("website").value = "";
     });
 
 
-    document.getElementById("buttonSortTitle").addEventListener("click", function () {
-        movieArray.sort(dynamicSort("Title"));
+    document.getElementById("websiteBtn").addEventListener("click", function () {
+        let websiteURL = document.getElementById("website").value;
+        if (websiteURL === "") {
+            alert("Website Not Available");
+        }
+        else {
+            window.open(websiteURL, "_blank");
+        }
+    });
+
+
+    // sorting options
+    let sortBy = document.getElementById("select-sortBy");
+
+    sortBy.addEventListener("change", function () {
+        if (sortBy.value == "sortName") {
+            sortByName();
+        }
+        if (sortBy.value == "sortCity") {
+            sortByCity();
+        }
+        if (sortBy.value == "sortStyle") {
+            sortByStyle();
+        }
+        if (sortBy.value == "sortHtoL") {
+            sortByHtoL();
+        }
+        if (sortBy.value == "sortLtoH") {
+            sortByLtoH();
+        }
+
         createList();
-        document.location.href = "index.html#ListAll";
+        document.location.href = "index.html#listPage";
     });
 
-    document.getElementById("buttonSortGenre").addEventListener("click", function () {
-        movieArray.sort(dynamicSort("Genre"));
-        createList();
-        document.location.href = "index.html#ListAll";
-    });
-// end of add button events ************************************************************************
-
-  
 
     // page before show code *************************************************************************
-    $(document).on("pagebeforeshow", "#ListAll", function (event) {   // have to use jQuery 
+    $(document).on("pagebeforeshow", "#listPage", function (event) {   // have to use jQuery 
         createList();
+        //createUpdatedList();
     });
- 
-// end of page before show code *************************************************************************
 
-});  
+    // need one for our details page to fill in the info based on the passed in ID
+    $(document).on("pagebeforeshow", "#detailPage", function (event) {
+        let restaurantDetailsList = localStorage.getItem("parm");  // get the unique key back from the storage dictionary called "parm"
+        document.getElementById("restaurantDetailsList").innerHTML = restaurantDetailsList;
+        document.getElementById("restaurantDetailsListOnUpdatedPage").innerHTML = restaurantDetailsList;
+
+        console.log(restaurantDetailsList);
+    });
+
+    // end of page before show code *************************************************************************
+    document.getElementById("updateBtn").addEventListener("click", function () {
+        let newDate = document.getElementById("dateUpdate").value;
+        let newMenu = document.getElementById("menuTriedUpdate").value;
+    
+        restaurantArray.push(new RestaurantObject ("", "", "", "", "", "", "", newDate, newMenu));
+    
+        console.log(restaurantArray);
+        document.getElementById("dateUpdate").value = "";
+        document.getElementById("menuTriedUpdate").value = "";
+    
+        let updatedRestaurantDetailsList = localStorage.getItem("updated");
+        document.getElementById("updatedRestaurantList").innerHTML = updatedRestaurantDetailsList;
+        document.getElementById("updatedRestaurantDetailsListOnUpdatedPage").innerHTML = updatedRestaurantDetailsList;
+        console.log(updatedRestaurantDetailsList);
+    
+        document.location.href = "index.html#detailPage";
+    });
+});
 // end of wait until document has loaded event  *************************************************************************
+
+
+// update button event
+//document.getElementById("updateBtn").addEventListener("click", updateList);
+
 
 function createList() {
     // clear prior data
-    var theList = document.getElementById("myul");
+    let theList = document.getElementById("myul");
     theList.innerHTML = "";
 
-    movieArray.forEach(function (element,) {   // use handy array forEach method
-        var li = document.createElement('li');
-        li.innerHTML =  element.ID + ":  " + element.Title + "  " + element.Genre;
+    restaurantArray.forEach(function (element,) {   // use handy array forEach method
+        let li = document.createElement('li');
+        li.classList.add('oneRestaurant');
+        li.innerHTML = element.Name + " / " + element.City + " / " + element.Style + " / " + element.Price;
+
+        // use the html5 "data-parm" to store the details of this particular restaurant object 
+        // that we are currently building an li for so that I can later know which restaurant this li came from
+        li.setAttribute("data-parm", "Name: " + element.Name + "<br><br>" +
+                                     "Cuisine Style: " + element.Style + "<br><br>" +
+                                     "Location: " + element.City + "<br><br>" +
+                                     "Price: " + element.Price + "<br><br><br>" +
+                                     "Date Visited: " + element.Date + "<br><br>" +
+                                     "Menu(s) Tried: " + element.Menu + "<br><br>");
+
+        li.setAttribute("data-updated", "Date Visited: " + element.NewDate + "<br><br>" +
+                                        "Menu(s) Tried: " + element.NewMenu + "<br><br>");
+
         theList.appendChild(li);
     });
-  
+
+
+    let classList = document.getElementsByClassName("oneRestaurant");
+    let newRestaurantArray = Array.from(classList);
+
+    newRestaurantArray.forEach(function (element,) {
+        element.addEventListener('click', function () {
+            let parm = this.getAttribute("data-parm");  // data-parm has this restaurant object's details
+            let updated = this.getAttribute("data-updated");  // data-parm has this restaurant object's details
+            localStorage.setItem("parm", parm);         // now save THIS DETAILS in the localStorage called "parm"
+            localStorage.setItem("updated", updated);
+            document.location.href = "index.html#detailPage";
+        });
+    });
+
 };
-  
 
-/**
- *  https://ourcodeworld.com/articles/read/764/how-to-sort-alphabetically-an-array-of-objects-by-key-in-javascript
-* Function to sort alphabetically an array of objects by some specific key.
-* 
-* @param {String} property Key of the object to sort.
-*/
-function dynamicSort(property) {
-    var sortOrder = 1;
 
-    if (property[0] === "-") {
-        sortOrder = -1;
-        property = property.substr(1);
-    }
+// function updateList() {
+//     let updatedList = new UpdatedRestaurantObject (
+//         document.getElementById("dateUpdate").value,
+//         document.getElementById("menuTriedUpdate").value
+//     );
+//     updatedRestaurantArray.push(updatedList);
+//     document.getElementById("dateUpdate").value = "";
+//     document.getElementById("menuTriedUpdate").value = "";
 
-    return function (a, b) {
-        if (sortOrder == -1) {
-            return b[property].localeCompare(a[property]);
-        } else {
-            return a[property].localeCompare(b[property]);
-        }
-    }
+//     // clear all list when user clicks "Show Movies" button
+//     document.getElementById("updatedRestaurantList").innerHTML = "";
+//     document.getElementById("updatedRestaurantDetailsListOnUpdatedPage").innerHTML = "";
+
+//     // create "unordered list" element 
+//     let liList = document.createElement('li');
+
+//     // convert "movieList" section into unordered list
+//     document.getElementById("updatedRestaurantList").appendChild(liList);
+//     document.getElementById("updatedRestaurantDetailsListOnUpdatedPage").appendChild(liList);
+
+//     for (let i in updatedRestaurantArray) {             
+//         let li = document.createElement('li');  // create "list" element   
+//         liList.appendChild(li);                     // add list to unordered list
+//         liList.innerHTML = updatedRestaurantArray[i].GetAll();  // display string output
+//     }
+// };
+
+
+// UpdatedRestaurantObject.prototype.GetAll = function() {
+//     return "Date Visited: " + this.NewDate + "<br><br>" +
+//            "Menu(s) Tried: " + this.NewMenu + "<br><br>";
+// };
+
+function sortByName() {
+    restaurantArray.sort(function (a, b) {
+        let x = a.Name.toLowerCase();
+        let y = b.Name.toLowerCase();
+        if (x < y) { return -1; }
+        if (x > y) { return 1; }
+        return 0;
+    });
+}
+
+function sortByCity() {
+    restaurantArray.sort(function (a, b) {
+        let x = a.City.toLowerCase();
+        let y = b.City.toLowerCase();
+        if (x < y) { return -1; }
+        if (x > y) { return 1; }
+        return 0;
+    });
+}
+
+function sortByStyle() {
+    restaurantArray.sort(function (a, b) {
+        let x = a.Style.toLowerCase();
+        let y = b.Style.toLowerCase();
+        if (x < y) { return -1; }
+        if (x > y) { return 1; }
+        return 0;
+    });
+}
+
+function sortByHtoL() {
+    restaurantArray.sort(function (a, b) {
+        let x = a.Price.toLowerCase();
+        let y = b.Price.toLowerCase();
+        if (x > y) { return -1; }
+        if (x < y) { return 1; }
+        return 0;
+    });
+}
+
+function sortByLtoH() {
+    restaurantArray.sort(function (a, b) {
+        let x = a.Price.toLowerCase();
+        let y = b.Price.toLowerCase();
+        if (x < y) { return -1; }
+        if (x > y) { return 1; }
+        return 0;
+    });
 }
